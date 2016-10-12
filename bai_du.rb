f1b1e3c9ea5eab32119092d7ae5643cb0345cff2
roccia <yanweizhen@yimeijian.cn>
2016-10-12 17:06:39 +0800

    input test

--- bai_du.rb
+++ bai_du.rb
@@ -5,89 +5,77 @@ require 'mechanize'
 require 'benchmark'
 require 'uri'
 
-
 class BaiDu
 
-  WORD = '东风日产'
-  TIME = '=1451520000,1472601600|stftype=1'
-  BASE_URI = "http://www.baidu.com/s?"
-  PerPage = 50
-  Agent = Mechanize.new
-  Agent.user_agent_alias = 'Mac Safari 4'
-   #Agent.set_proxy('182.90.252.10', '2226')
-  Agent.follow_meta_refresh = true
+  Agent = Mechanize.new{|a|
+    a.follow_meta_refresh = true
+    a.user_agent_alias = 'Mac Safari 4'
+    a.keep_alive=false
+    a.open_timeout   = 5
+    a.read_timeout   = 5
+    a.follow_meta_refresh = true
+  }
 
-  def self.query(word)
-    q = Array.new
-    q << "wd=#{word}"
-    q << "rn=#{PerPage}"
-    q << "gpc=stf#{URI.encode(TIME)}"
-    query_str = q.join("&")
-    uri = URI.encode(BASE_URI+ query_str)
-    uri
-  end
 
-  def self.generate_url
-    words = []
-    sites = []
-    all_url = []
-
-    #File.read("words.txt").each_line{|line| words << line.chop}
-
-    File.read("sites.txt").each_line{|line|  sites << line.chop}
-     puts sites.size
-      sites.each do |s|
-        all_url << "http://www.baidu.com/s?wd=intitle:#{URI.encode(WORD)}%20site:%20#{s}&rn=50&gpc=stf#{URI.encode(TIME)}"
-
-    end
-    all_url
-  end
+  # def self.generate_url
+  #   words = []
+  #   sites = []
+  #   all_url = []
+  #
+  #   File.read("words.txt").each_line{|line| words << line.chop}
+  #
+  #   File.read("sites.txt").each_line{|line|  sites << line.chop}
+  #     sites.each do |s|
+  #       all_url << "http://www.baidu.com/s?wd=intitle:#{URI.encode(WORD)}%20site:%20#{s}&rn=50&gpc=stf#{URI.encode(TIME)}"
+  #
+  #   end
+  #   all_url
+  # end
 
   $total_time_begin = Time.now.to_i
 
-  def self.generate_page
+  def self.generate_page(uri)
     queue = Queue.new
-    #threads = []
-    generate_url.each{|u| queue.push(u) }
-    #threadNums = 1000
-    #threadNums.times do
-      #threads<<Thread.new do
-        until queue.empty?
-          url = queue.pop(true) rescue nil
-          first_page = Agent.get(url)
-          rs = get_all_pages(first_page)
-            if no_result(first_page) == nil
-              return '无数据'
-            end
+     queue.push(uri)
+    until queue.empty?
+      url = queue.pop(true) rescue nil
+      first_page = Agent.get(url)
+      rs = get_all_pages(first_page)
 
-            if one_page(first_page) == nil
-             save_to get_real_urls(single_page(first_page))
-            end
-            if rs.class == Array
-                  rs.each{|s|  save_to "#{get_real_urls(s)} \n"
-                   }
-            else
-                #puts get_real_urls(rs) unless rs != ' '
-               save_to "#{get_real_urls(rs)} \n" unless rs != ' '
-            end
+      if no_result(first_page) == nil
+        return '无数据'
+      end
 
-     #    end
-     #  end
-     #
-     # threads.each{|t| t.join}
+      if one_page(first_page) == false
+        single_page(first_page).each{|s|
+
+          save_to "#{get_real_urls(s)} \n"}
+      end
+
+      if rs.class == Array
+        rs.each{|s|
+
+          save_to "#{get_real_urls(s)} \n"
+         }
+      else
+
+           save_to "#{get_real_urls(rs)} \n" unless rs != ' '
+      end
+
     end
 
     $total_time_end = Time.now.to_i
-   # puts "线程数：" + threadNums.to_s
     puts "执行时间：" + ($total_time_end - $total_time_begin).to_s + "秒"
   end
 
+
   def self.save_to(data)
 
-    File.open("test1.txt", "a+") do |f|
+    File.open("#{$keywords}_result.txt", "a+") do |f|
       f.write(data)
     end
   end
+
 
   def self.click_to_next_page(page)
     Agent.click(page.link_with(:text=>/下一页/)) unless page.link_with(:text=>/下一页/).nil?
@@ -102,10 +90,7 @@ class BaiDu
       np = click_to_next_page(np)
       np && results.push(np)
     end
-
     results.push(page)
-    results
-
     results.each do |rs|
       return '' if rs.nil?
       rs.search('//div[@class="result c-container "]//h3[@class="t"]/a').each do |p|
@@ -114,7 +99,6 @@ class BaiDu
 
     end
      url_arys
-
   end
 
   def self.get_all_urls(page)
@@ -134,7 +118,7 @@ class BaiDu
  end
 
  def self.one_page(page)
-   page.search('//div[@id="page"]')
+   page.search('//div[@id="page"]').empty?
  end
 
  def self.single_page(page)
@@ -142,25 +126,33 @@ class BaiDu
    page.search('//div[@class="result c-container "]//h3[@class="t"]/a').each do |p|
      url_arys << p['href']
    end
-   url_arys
+    url_arys
  end
 
-  def self.get_real_urls(uri)
-    RestClient.get(uri){ |response, request, result, &block|
-      if [301, 302, 307].include? response.code
+    def self.get_real_urls(uri)
+
+    site = RestClient::Resource.new(uri, :verify_ssl =>  OpenSSL::SSL::VERIFY_PEER)
+    site.get{ |response|
+
         redirected_url = response.headers[:location]
-      else
-        response.return!(request, result, &block)
-      end
-      redirected_url
+        redirected_url
     }
 
   end
 
 
- puts generate_page
+ def self.final_result
+  url = []
+  s = File.read("#{$keywords}.txt").each_line{|line| url << line.chop}
+    url.each do |u|
+     generate_page(u)
+    end
+ end
 
-  #File.open('test2.txt', 'w') { |file| file.write(rs) }
+  print "keywords: "
+  $keywords = gets.strip
+  puts $keywords
+  puts final_result
 end
 
 
--- get_result.rb
+++ get_result.rb
@@ -1,27 +1,55 @@
-
+require 'mechanize'
+require 'nokogiri'
 require 'rest-client'
+ require 'pathname'
 
 class GetResult
+  TIME = '=1451520000,1472601600|stftype=1'
+  def get_words
+    words = []
+    threads =[]
+    queue = Queue.new
+    File.read("words.txt").each_line{|line| words << line.chop}
+    words.each{|w| queue.push w}
+    words.each do
+      threadsnums = 10
+       threadsnums.times do
+         threads << Thread.new do
+            until queue.empty?
+               word = queue.pop(true) rescue nil
+              save_to(word,"#{generate_url(word)}")
 
-    def result
+            end
+         end
+       end
+      threads.each{|t| t.join}
+    end
 
-      redirected_urls =[]
-    #s = BaiDu.get_real_urls(rs)
-     rs =  File.read('test1.txt').split("[]")
-      rs.each do |s|
-      RestClient.get(s){ |response, request, result, &block|
-        if [301, 302, 307].include? response.code
-          redirected_url = response.headers[:location]
-        else
-          response.return!(request, result, &block)
-        end
-          redirected_url.class
-        redirected_url
-      }
-     end
-   end
+  end
+
+  def save_to(name,data)
+
+    File.open("#{name}.txt", "a+") do |f|
+      f.write(data)
+    end
+  end
+
+  def  generate_url(word)
+    sites = []
+    all_url = []
+    File.read("sites.txt").each_line{|line|  sites << line.chop}
+    sites.each do |s|
+      all_url << "http://www.baidu.com/s?wd=intitle:#{URI.encode(word)}%20site:%20#{s}&rn=50&gpc=stf#{URI.encode(TIME)}"
+    end
+    all_url.join("\n")
+  end
+
+
 end
 
 
-    r = GetResult.new
-    puts r.result.size
+
+
+
+
+
